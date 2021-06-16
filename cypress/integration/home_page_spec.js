ca8f66162b8160ab167e05c4f018dea6bd1848ca
jane.fwp <cuij1012@gmail.com>
2021-06-16 12:54:00 +1000

    add integration test case

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,14 @@
 describe('The Home Page', () => {
     it('successfully loads', () => {
       cy.visit('/') // change URL to match your dev URL
+      cy.get('.btn').click() //click "Create a new todo" button
+      cy.get('.modal')
+        .get('form')
+        .get('input:first').type('aaa')
+        .get('select').select('css')
+        .get('textarea').type('just for test')
+        .get('input:last').click().clock(new Date())
+        .get('form').submit()
     })
+    
   })
