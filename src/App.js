618849bc3b41a2f10e4cf846ce572a7ae0ca2d65
janefwp <cuij1012@gmail.com>
2021-08-22 14:42:54 +1000

    for deploy

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, {Suspense} from 'react'
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import HomeScreen from '../src/screens/homescreens/HomeScreen'
-import AboutScreen from '../src/screens/aboutscreens/AboutScreen'
+import HomeScreen from './screens/homescreens/HomeScreen'
+import AboutScreen from './screens/aboutscreens/AboutScreen'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import InfodetailScreen from './screens/infodetailscreens/InfodetailScreen'
